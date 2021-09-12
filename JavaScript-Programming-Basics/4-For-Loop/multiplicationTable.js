e0ec6d4c3b95cd1aac64d79698e1c9c1731fc77d
Kiril Valkov <kiril_dgr@abv.bg>
2021-09-12 09:41:39 +0300

    Added multiplicationTable.js

---
+++ JavaScript-Programming-Basics/4-For-Loop/multiplicationTable.js
@@ -0,0 +1,10 @@
+function multiplicationTable(input) {
+    let number = Number(input[0]);
+
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${i} * ${number} = ${number * i}`)
+    }
+
+}
+
+multiplicationTable(["5"]);
