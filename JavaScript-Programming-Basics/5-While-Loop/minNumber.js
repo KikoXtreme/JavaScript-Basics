49e5d936574b6859455ba09a8d5d7110cdd4fe60
Kiril Valkov <kiril_dgr@abv.bg>
2021-09-18 11:05:11 +0300

    Added minNumber.js

---
+++ JavaScript-Programming-Basics/5-While-Loop/minNumber.js
@@ -0,0 +1,17 @@
+function minNumber(input) {
+    let index = 0;
+    let command = input[index++];
+    let min = Number.MAX_SAFE_INTEGER;
+
+    while (command !== 'Stop') {
+        let currentNum = Number(command);
+
+        if (min > currentNum) {
+            min = currentNum;
+        }
+        command = input[index++];
+    }
+    console.log(min);
+}
+
+minNumber(["100", "99", "80", "70", "Stop"]);
